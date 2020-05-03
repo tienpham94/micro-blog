63918dafd1c40e212af13e6a9b46926f802ae1f4
Tien Pham <tiendev94@gmail.com>
2020-05-03 21:39:28 +0300

    mockup

---
+++ client/src/PostCreate.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <div>
+      <form className="form-group">
+        <label>Title</label>
+        <input className="form-control" />
+      </form>
+      <button className="btn btn-primary">Submit</button>
+    </div>
+  )
+}
