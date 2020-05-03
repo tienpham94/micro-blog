e8cd18e42836115b95d2cf4684a4af71fd00503f
Tien Pham <tiendev94@gmail.com>
2020-05-03 22:25:34 +0300

    state

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 export default () => {
+  const [posts, setPosts] = useState({})
+
   return <div></div>
 };
